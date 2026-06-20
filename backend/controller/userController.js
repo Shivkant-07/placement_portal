f124543912761fe3509f9a6cac127ff30c971ef5
shivkant kurmi <pshivkantpatel@gmail.com>
2026-06-20 20:49:56 +0530

    Update CORS origin for frontend

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -76,10 +76,11 @@ export const login = async (req, res) => {
         )
 
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            sameSite: "lax"
-        })
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 din (days * hours * mins * secs * millisecs)
+})
 
         res.send({
             message: "Login successful",
